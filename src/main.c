319d395e8d6a9b5157069724fdac0cd25c6c156a
raphico <raphicogit@gmail.com>
2025-08-09 18:12:38 +0100

    feat: handle invalid commands

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -11,5 +11,8 @@ int main(int argc, char *argv[]) {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
+  input[strcspn(input, "\n")] = '\0';
+  printf("%s: command not found\n", input);
   return 0;
 }
