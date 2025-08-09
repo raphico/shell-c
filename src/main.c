e408a76a3bd1277411261406fcce92bace2d9115
raphico <raphicogit@gmail.com>
2025-08-09 18:17:36 +0100

    feat: REPL

--- src/main.c
+++ src/main.c
@@ -2,17 +2,16 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
+  while (1) {
+    printf("$ ");
+    char input[100];
+    fgets(input, 100, stdin);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+    input[strcspn(input, "\n")] = '\0';
+    printf("%s: command not found\n", input);
+  }
 
-  input[strcspn(input, "\n")] = '\0';
-  printf("%s: command not found\n", input);
   return 0;
 }
