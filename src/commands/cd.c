07d9df0bed8cda3c6f274a65f20b4981b503635d
raphico <raphicogit@gmail.com>
2025-08-10 14:56:52 +0100

    feat: implement home directory for cd builtin

--- src/commands/cd.c
+++ src/commands/cd.c
@@ -1,11 +1,22 @@
 #include "shell.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 int exec_cd(int argc, char **argv) {
     if (argc > 2) {
         printf("cd: too many arguments\n");
         return -1;
+    }
+
+    if (strcmp(argv[0], "~") == 0) {
+        char *home_dir = getenv("HOME");
+        if (!home_dir) {
+            return -1;
+        }
+
+        return chdir(home_dir);
     }
 
     if (chdir(argv[0]) < 0) {
